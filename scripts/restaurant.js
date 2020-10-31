eb001d0d9c294526c68b8f0127cf90e1e4441fac
Reo Tamai <reotamai5@gmail.com>
2020-10-31 14:45:36 -0700

    updated header and footer to be clickable. Updated restaurant.html

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -1,3 +1,21 @@
 /******************************************/
 /* This is javascript for restaurant.html */
 /******************************************/
+$(document).ready(function() {
+
+  var stars = 4;
+  setStar(3);
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+});
+
+
+function setStar(stars){
+  if(stars <= 5 && stars >=0){
+    for(i = 0; i <= stars; i++){
+      $("#star"+i).attr("src","images/darkStar.png");
+    }
+  }
+}
