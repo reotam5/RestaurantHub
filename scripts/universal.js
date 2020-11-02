c5901dc638c2f2919657b0d8e0d66caa4ffc981a
Reo Tamai <reotamai5@gmail.com>
2020-11-01 23:57:45 -0800

    Any page can import header and slide bar by adding required sectinon to header element

--- scripts/universal.js
+++ scripts/universal.js
@@ -5,10 +5,32 @@
 
 
 $(document).ready(function() {
+  /*******************************************/
+  /* Jquery for putting universal components */
+  /*******************************************/
+  $.ajax({
+    url: "universalContent.html",
+    cache: false,
+    datatype: 'html',
+    success: function(html) {
+        var result = $(html);
+        if(!$("body").hasClass("NoUniversal")){
+          var paddingTarget = $("body").children().first();
+          var addPadding = parseFloat(paddingTarget.css("margin-top")) + 60 + "px"
+          paddingTarget.css("padding-top",addPadding);
+          var $dom = $(document.createElement("html"));
+          $dom[0].innerHTML = html;
+          var body = $dom.find("body");
+          $("body").prepend(body.children());
+        }else{
+          console.log("This page's body has 'NoUniversal' class. Therefore no universal components were added.");
+        }
+    }
+  });
+
   /****************************/
   /* Jquery for navbar starts */
   /****************************/
-  $(".slidebar").next().addClass("adoptNav");
 
   $(window).scroll(function() {
     if($(window).scrollTop() != 0){
@@ -17,17 +39,17 @@ $(document).ready(function() {
       $(".siteHead").removeClass("transform");
     }
   });
+
   $(document).on("click", function(event) {
     var target = $(event.target);
     if (target.is(".btn-slide")) {
       $('.slidebar').toggleClass('openNav');
-    }else if(target.is(".nav")){
+    }else if(target.is(".slidebar")){
       $('.slidebar').addClass('openNav');
     }else{
       $(".slidebar").removeClass("openNav");
     }
   });
-
   /**************************/
   /* Jquery for navbar ends */
   /**************************/
