c43aa2889891ef8bb8accc9a3655febcb392a591
Sam Lando <slandolando@hotmail.com>
2020-11-05 18:38:00 -0800

    Fixed searchbar responsiveness and added Sortby button

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@
 /* This is javascript for main.html */
 /******************************************/
 $(document).ready(function () {
-  let listHours1 = "<p>Monday - 10:00AM - 10:00PM</p>"
-                 + "<p>Tuesday - 10:00AM - 10:00PM</p>"
-                 + "<p>Wednesday - Closed</p>"
-                 + "<p>Thursday - 10:00AM - 10:00PM</p>"
-                 + "<p>Friday - 10:00AM - 10:00PM</p>"
-                 + "<p>Saturday - 10:00AM - 10:00PM</p>"
-                 + "<p>Sunday - 12:00AM - 8:00PM</p>"
+  let listHours1 = "<p>Monday - 10:00AM - 10:00PM</p>" +
+    "<p>Tuesday - 10:00AM - 10:00PM</p>" +
+    "<p>Wednesday - Closed</p>" +
+    "<p>Thursday - 10:00AM - 10:00PM</p>" +
+    "<p>Friday - 10:00AM - 10:00PM</p>" +
+    "<p>Saturday - 10:00AM - 10:00PM</p>" +
+    "<p>Sunday - 12:00AM - 8:00PM</p>"
   let rest1 = ["keg", "/images/keg.jpg", true, true, false, listHours1]
   createPanel(rest1[0], rest1[1], rest1[2], rest1[3], rest1[4], rest1[5]);
   createPanel("joeys", rest1[1], rest1[2], rest1[3], rest1[4], rest1[5]);
@@ -29,3 +29,31 @@ function createPanel(name, img, reserve, tracker, verified, hours) {
   }
   $(x + " .restaurant-details .hours").append(hours);
 }
+
+//Temporary/scuffed script for sortby dropdown active class
+
+$('#sortby1').on('click', function (e) {
+  $('#sortby1').each(function () {
+    $(this).addClass('active');
+    $("#sortby2").removeClass('active');
+    $("#sortby3").removeClass('active');
+  })
+
+});
+
+$('#sortby2').on('click', function (e) {
+  $('#sortby2').each(function () {
+    $(this).addClass('active');
+    $("#sortby1").removeClass('active');
+    $("#sortby3").removeClass('active');
+  })
+
+  $('#sortby3').on('click', function (e) {
+    $('#sortby3').each(function () {
+        $(this).addClass('active');
+        $("#sortby1").removeClass('active');
+        $("#sortby2").removeClass('active');
+    })
+
+});  
+});
