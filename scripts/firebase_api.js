abe4fc7723dabe0686c57a2b56fead1ad1e81252
Reo Tamai <reotamai5@gmail.com>
2020-11-06 11:05:22 -0800

    added firebase api

---
+++ scripts/firebase_api.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC3U6iIEF9vhC04kjYzFNDKesyOC746jPs",
+  authDomain: "restaurant-hub-451af.firebaseapp.com",
+  databaseURL: "https://restaurant-hub-451af.firebaseio.com",
+  projectId: "restaurant-hub-451af",
+  storageBucket: "restaurant-hub-451af.appspot.com",
+  messagingSenderId: "749944603047",
+  appId: "1:749944603047:web:a7bdb37a30dc29a622f682"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
