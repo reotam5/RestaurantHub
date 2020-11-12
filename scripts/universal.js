8dad911b1e124c238a0e8303e75d90969b393ce5
Hyunjin Nha <hyunjinnha@gmail.com>
2020-11-11 19:56:50 -0800

    added firebase to all html pages.

--- scripts/universal.js
+++ scripts/universal.js
@@ -43,18 +43,18 @@ $(document).ready(function() {
 //$('body').children().filter(".loading-bg").delay(900).fadeOut(800);  
 });
 
-var loggedin = false;
-var user;
+// var loggedin = false;
+// var user;
 
-firebase.auth().onAuthStateChanged(function(me) {
-  if (me) {
-    user = me;
-    loggedin = true;
-  } else {
-    user = null;
-    loggedin = false;
-  }
-});
+// firebase.auth().onAuthStateChanged(function(me) {
+//   if (me) {
+//     user = me;
+//     loggedin = true;
+//   } else {
+//     user = null;
+//     loggedin = false;
+//   }
+// });
 
 /************************************/
 /* Check status of logged in or not */
@@ -72,7 +72,7 @@ initApp = function() {
       // var providerData = user.providerData;
       user.getIdToken().then(function(accessToken) {
         document.getElementById('sign-in-status').textContent = 'Signed in';
-        document.getElementById('sign-in').textContent = 'Sign out';
+        $(".sign-in").text("Sign out");
         document.getElementById('account-details').textContent = JSON.stringify({
           displayName: displayName,
           email: email,
