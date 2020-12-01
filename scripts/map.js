a82da511c8d8707f4471da0fca2436828e8c3e80
Hyunjin Nha <hyunjinnha@gmail.com>
2020-12-01 15:31:33 -0800

    added marker to map and default location if permission denied

--- scripts/map.js
+++ scripts/map.js
@@ -1,18 +1,48 @@
 /***********************************/
 /* This is javascript for map.html */
 /***********************************/
-$(document).ready(function() {
-  getLocation();
+
+
+$( window ).on("load", async function() {
+   await getLocation()
 });
+
+var lng;
+var lat;
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, positionError);
   } else {
-    null;
+    console.log("error has occured");
   }
 }
 
 function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  runMap(lng, lat);
+};
 
+function positionError(error) {
+  if (error.code == error.PERMISSION_DENIED || error.code == error.POSITION_UNAVAILABLE) {
+    var lat = 49.24;
+    var lng = -123.11;
+    runMap(lng, lat);
+  } else  {
+    console.log("Unknown error");
+  }
+}
+
+function runMap(lng, lat) {
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoia2V2aW5uaGE5NSIsImEiOiJja2hnbzV4dzcwb3BzMndvanlhZmpyZW55In0.Q2_WdnutfaDh6-i1NPWKJQ";
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [lng, lat], // starting position [lng, lat]
+    zoom: 10 
+  })
+
+  var marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
 }
